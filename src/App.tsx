2baf1de37aef9151a64122d911575289df25718c
Henrique M. N. <henriquemancininevado@gmail.com>
2024-01-17 10:22:41 -0300

    Instalação e configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
-import Home from './pages/home/Home';
-
+import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 function App() {
   return (
     <>
-    <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
